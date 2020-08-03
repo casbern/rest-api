0dd6c63201816a399500f42d305fc38ca7db8fd7
Cássia <cascrisbern@gmail.com>
2020-08-03 19:42:27 -0300

    making request with curl

--- routes.js
+++ routes.js
@@ -2,7 +2,7 @@ const express = require('express')
 const routes = express.Router()
 const Courses = require('./courses')
 
-routes.get('/', Courses.all)
+routes.get('/courses', Courses.all)
 routes.post('/courses/create', Courses.create)
 routes.put('/courses/:id', Courses.update)
 routes.delete('/courses/:id', Courses.delete)
